98041c1bd48b43f06973332cc4fb19e7ce0d502c
Filip Danilovski <filip_danilovski@hotmail.com>
2022-08-26 23:25:35 +0200

    Create subcategory model and update view

--- KnowTheWorld/app/models.py
+++ KnowTheWorld/app/models.py
@@ -4,3 +4,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
     name = models.CharField(max_length=50, primary_key=True)
+
+class Subcategory(models.Model):
+    class Meta:
+        verbose_name_plural = "subcategories"
+    name = models.CharField(max_length=50, primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- KnowTheWorld/app/views.py
+++ KnowTheWorld/app/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Subcategory
 
 def index(request):
     categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
     context = {'categories': categories}
     return render(request,'app/index.html', context)
