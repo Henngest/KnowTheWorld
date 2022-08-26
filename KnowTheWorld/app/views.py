e21462b22fce01155f10841f95bdccaf8e787b7b
Filip Danilovski <filip_danilovski@hotmail.com>
2022-08-27 00:02:12 +0200

    Create view and links for subcategories

--- KnowTheWorld/app/views.py
+++ KnowTheWorld/app/views.py
@@ -2,8 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Category, Subcategory
 
+
 def index(request):
     categories = Category.objects.all()
-    subcategories = Subcategory.objects.all()
     context = {'categories': categories}
-    return render(request,'app/index.html', context)
+    return render(request, 'app/index.html', context)
+
+def subcategory(request, category_id, subcategory_id):
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    subcategory = Subcategory.objects.get(pk=subcategory_id)
+    context = {'category': category,
+               'subcategory': subcategory,
+               'categories': categories}
+    return render(request, 'app/subcategory.html', context)
