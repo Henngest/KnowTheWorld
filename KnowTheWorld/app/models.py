6bf9f7bb3b1f62114bd020fc602f7ca06fb46635
Filip Danilovski <filip_danilovski@hotmail.com>
2022-08-27 21:30:41 +0200

    Create model and view for lessons

--- KnowTheWorld/app/models.py
+++ KnowTheWorld/app/models.py
@@ -10,3 +10,8 @@ class Subcategory(models.Model):
         verbose_name_plural = "subcategories"
     name = models.CharField(max_length=50, primary_key=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Lesson(models.Model):
+    text = models.TextField()
+    image = models.CharField(max_length=200)
+    lesson_subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
