26aadab2fd5f6715e84840d654a1fae6610f06dd
Filip Danilovski <filip_danilovski@hotmail.com>
2022-09-05 17:10:24 +0200

    Add correct answers to quiz view

--- KnowTheWorld/app/views.py
+++ KnowTheWorld/app/views.py
@@ -84,6 +84,7 @@ def show_quiz(request, category_id, subcategory_id, quiz_level):
         total = 0
         quizz = subcategory.quiz_set.get(quiz_level=level)
         questions = Question.objects.filter(quiz__id=quizz.id)
+        list_answers = []
         for q in questions:
             choices = Choice.objects.filter(question__question_text=q.question_text)
             total += 1
@@ -91,6 +92,8 @@ def show_quiz(request, category_id, subcategory_id, quiz_level):
                 if c.is_correct_choice:
                     if c.choice_text == request.POST.get(q.question_text):
                         correct += 1
+                    else:
+                        list_answers.append(total.__str__()+" is "+c.choice_text)
         result = round(correct / total * 100, 2)
         sqr = QuizResults()
         flag = True
@@ -109,7 +112,8 @@ def show_quiz(request, category_id, subcategory_id, quiz_level):
                    'categories': categories,
                    "subcategory": subcategory,
                    "quiz_level": level,
-                   "result": result}
+                   "result": result,
+                   "list_answers": list_answers}
         return render(request, template_name="app/quiz.html", context=context)
     context = {"category": category,
                'categories': categories,
